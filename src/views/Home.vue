821a4583931c92175e9cf80c62bbd62b1dbce92c
Simeon Cheuk <etsimeon2030@gmail.com>
2021-09-03 10:53:58 +1000

    added and comment the board

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,42 +5,79 @@
         <ion-title>Blank</ion-title>
       </ion-toolbar>
     </ion-header>
-    
+
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">Blank</ion-title>
         </ion-toolbar>
       </ion-header>
-    
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+
+      <div id="app">
+        <h1>Simple Chessboard with legal</h1>
+        <!-- <chessboard /> -->
       </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script>
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-import { defineComponent } from 'vue';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/vue";
+import { defineComponent } from "vue";
+// import { chessboard } from "vue-chessboard";
+// import "vue-chessboard/dist/vue-chessboard.css";
 
 export default defineComponent({
-  name: 'Home',
+  name: "Home",
   components: {
+    // chessboard,
     IonContent,
     IonHeader,
     IonPage,
     IonTitle,
-    IonToolbar
-  }
+    IonToolbar,
+  },
+
+  data() {
+    return {
+      currentFen: "",
+      positionInfo: null,
+    };
+  },
+  methods: {
+    showInfo(data) {
+      this.positionInfo = data;
+    },
+    loadFen(fen) {
+      this.currentFen = fen;
+    },
+    promote() {
+      if (confirm("Want to promote to rook? Queen by default")) {
+        return "r";
+      } else {
+        return "q";
+      }
+    },
+  },
+  created() {
+    this.fens = [
+      "5rr1/3nqpk1/p3p2p/Pp1pP1pP/2pP1PN1/2P1Q3/2P3P1/R4RK1 b - f3 0 28",
+      "r4rk1/pp1b3p/6p1/8/3NpP2/1P4P1/P2K3P/R6R w - - 0 22",
+    ];
+  },
 });
 </script>
 
 <style scoped>
 #container {
   text-align: center;
-  
+
   position: absolute;
   left: 0;
   right: 0;
@@ -56,9 +93,9 @@ export default defineComponent({
 #container p {
   font-size: 16px;
   line-height: 22px;
-  
+
   color: #8c8c8c;
-  
+
   margin: 0;
 }
 
